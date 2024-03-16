f2d038071f0eeaac6edc0a3d9eacad5ec39857d1
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-03-16 13:45:28 +0100

    Added rest api framework and adjusted settings

--- jenkins_scraper/urls.py
+++ jenkins_scraper/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+from rest_api import urls as api_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(api_urls)),
 ]
