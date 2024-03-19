ed863d33a21037112cced33ee7e8d95cb9298505
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-03-19 12:17:20 +0100

    Added builds scraper functionality

--- scrap_info_script/constants.py
+++ scrap_info_script/constants.py
@@ -1,2 +1,3 @@
-dbFilename = 'db.sqlite3'
-japcoApiAccessPath = '~/.jenkins_token'
+db_filename = 'db.sqlite3'
+jenkins_api_token = '.jenkins_token'
+jenkins_base_url = 'http://localhost:8080/'
--- scrap_info_script/scrapBuildsToDb.py
+++ scrap_info_script/scrapBuildsToDb.py
@@ -4,6 +4,7 @@ import sqlite3
 import requests
 import argparse
 import datetime
+import constants
 
 # Disable urllib3 warnings and debug messages
 requests.packages.urllib3.disable_warnings()
@@ -30,6 +31,9 @@ log.addHandler(fh)
 log.addHandler(ch)
 
 
+jobs_dict = {}
+
+
 def parseArgumentStr(exclude_str):
     return exclude_str.split(',') if exclude_str else []
 
@@ -43,7 +47,7 @@ def parseArguments(argument):
 
 def scrap_jenkins_info(url):
     try:
-        response = requests.get(url)
+        response = requests.get(f'{url}/api/python')
         response.raise_for_status()
         if response.ok:
             jenkins_info = ast.literal_eval(response.text)
@@ -52,8 +56,30 @@ def scrap_jenkins_info(url):
         log.error(f'Exception occurred: {e}.')
 
 
+def get_jobs_info_into_dict(jobs):
+    for job in jobs:
+        job_info = scrap_jenkins_info(job['url'])
+        job_name = job_info['name']
+        job_builds = job_info['builds']
+        if job_name not in jobs_dict:
+            jobs_dict[job_name] = []
+        for build in job_builds:
+            build_git_sha = None
+            build_info = scrap_jenkins_info(build['url'])
+            for action in build_info['actions']:
+                if 'buildsByBranchName' in action:
+                    build_git_sha = action['buildsByBranchName']['refs/remotes/origin/main']['marked']['SHA1']
+            jobs_dict[job_name].append({'build_number': build_info['number'],
+                                        'build_url': build_info['url'],
+                                        'build_result': build_info['result'],
+                                        'build_timestamp': build_info['timestamp'],
+                                        'build_git_sha': build_git_sha})
+
+
 def main():
-    scrap_jenkins_info('http://localhost:8080/api/python')
+    jobs = scrap_jenkins_info(constants.jenkins_base_url)['jobs']
+    get_jobs_info_into_dict(jobs)
+    print(jobs_dict)
 
 
 if __name__ == '__main__':
