d511541dbb4ab54f9f233607beb64f857c62affc
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-03-19 13:52:11 +0100

    Added models

--- main_app/jenkins_statistics/models.py
+++ main_app/jenkins_statistics/models.py
@@ -1,3 +1,23 @@
+import django.utils
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    job_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.job_name
+
+
+class Build(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    build_number = models.IntegerField(default=1)
+    build_timestamp = models.DateTimeField(default=django.utils.timezone.now)
+
+
+class JobResults(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    build = models.ForeignKey(Build, on_delete=models.CASCADE)
+    build_url = models.CharField(max_length=100)
+    build_result = models.IntegerField(default=1)
+    build_git_sha = models.CharField(max_length=100)
