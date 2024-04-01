fa9cfe30e6a52c911f76b61d6d4e89af439ffe44
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-04-01 11:19:37 +0200

    Update gather_statistics_to_db.py

--- scrap_info_script/gather_statistics_to_db.py
+++ scrap_info_script/gather_statistics_to_db.py
@@ -7,6 +7,7 @@ import requests
 import argparse
 import datetime
 from enum import Enum
+from bs4 import BeautifulSoup
 
 import constants
 
@@ -19,7 +20,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--log_file', action='store_true', help='Option to save logs to file.')
 
 args = parser.parse_args()
-
 
 formatter = logging.Formatter('[%(levelname)s] %(message)s')
 log = logging.getLogger()
@@ -42,17 +42,6 @@ class Result(Enum):
     UNKNOWN = 2
 
 
-def parseArgumentStr(exclude_str):
-    return exclude_str.split(',') if exclude_str else []
-
-
-def parseArguments(argument):
-    arguments = parseArgumentStr(argument)
-    split_args = '|'.join(arguments)
-    regex = fr'.*({split_args}).*' if split_args else r'\*'
-    return regex
-
-
 def scrap_jenkins_info(url):
     try:
         response = requests.get(f'{url}/api/python')
@@ -70,7 +59,7 @@ def insert_to_db_by_api(url, data):
 
     while not success:
         try:
-            response = requests.post(url, json=data, verify=False)
+            response = requests.post(url, json=data)
             if not response.ok:
                 raise Exception(response.text)
             success = True
@@ -84,9 +73,17 @@ def insert_to_db_by_api(url, data):
     return response
 
 
-def get_job_id_from_db(job_name):
-    site_jobs_rest_api = f'{constants.site_jobs_rest_api}?job_name={job_name}&format=json'
-    response = requests.get(site_jobs_rest_api, verify=False)
+def get_pipeline_id_from_db(pipeline_name):
+    site_pipelines_rest_api = f'{constants.site_pipelines_rest_api}?pipeline_name={pipeline_name}&format=json'
+    response = requests.get(site_pipelines_rest_api)
+    pipeline_info = json.loads(response.text)
+    pipeline_id = pipeline_info[0].get('id') if response.ok and pipeline_info else None
+    return pipeline_id
+
+
+def get_job_id_from_db(pipeline_id, job_name):
+    site_jobs_rest_api = f'{constants.site_jobs_rest_api}?pipeline_id={pipeline_id}&job_name={job_name}&format=json'
+    response = requests.get(site_jobs_rest_api)
     job_info = json.loads(response.text)
     job_id = job_info[0].get('id') if response.ok and job_info else None
     return job_id
@@ -94,24 +91,37 @@ def get_job_id_from_db(job_name):
 
 def get_build_id_from_db(job_id, build_number):
     site_builds_rest_api = f'{constants.site_builds_rest_api}?job_id={job_id}&build_number={build_number}&format=json'
-    response = requests.get(site_builds_rest_api, verify=False)
+    response = requests.get(site_builds_rest_api)
     build_info = json.loads(response.text)
     build_id = build_info[0].get('id') if response.ok and build_info else None
     return build_id
 
 
-def get_job_result_id_from_db(job_id, build_id):
-    site_job_results_rest_api = f'{constants.site_job_results_rest_api}?job_id={job_id}&build_id={build_id}&format=json'
-    response = requests.get(site_job_results_rest_api, verify=False)
+def get_job_result_id_from_db(pipeline_id, job_id, build_id):
+    site_job_results_rest_api = f'{constants.site_job_results_rest_api}?pipeline_id={pipeline_id}&job_id={job_id}&build_id={build_id}&format=json'
+    response = requests.get(site_job_results_rest_api)
     job_result_info = json.loads(response.text)
     job_result_id = job_result_info[0].get('id') if response.ok and job_result_info else None
     return job_result_id
 
 
-def insert_into_jobs_table(job_name):
-    job_id = get_job_id_from_db(job_name)
+def insert_into_pipelines_table(pipeline_name):
+    pipeline_id = get_pipeline_id_from_db(pipeline_name)
+    if not pipeline_id:
+        data = {'pipeline_name': pipeline_name}
+        response = insert_to_db_by_api(constants.site_pipelines_rest_api, data)
+        if not response.ok:
+            raise Exception(f'Couldn\'t insert data: {data} to pipelines table. '
+                            f'Response: {response}, {response.content}.')
+        pipeline_id = json.loads(response.text)['id']
+        log.info(f'New pipeline detected, created entry for: {pipeline_name}.')
+    return pipeline_id
+
+
+def insert_into_jobs_table(pipeline_id, job_name):
+    job_id = get_job_id_from_db(pipeline_id, job_name)
     if not job_id:
-        data = {'job_name': job_name}
+        data = {'pipeline_id': pipeline_id, 'job_name': job_name}
         response = insert_to_db_by_api(constants.site_jobs_rest_api, data)
         if not response.ok:
             raise Exception(f'Couldn\'t insert data: {data} to jobs table. '
@@ -126,7 +136,7 @@ def insert_into_builds_table(job_id, build_number, build_timestamp):
     if not build_id:
         data = {'job_id': job_id,
                 'build_number': build_number,
-                'build_timestamp': datetime.datetime.fromtimestamp(build_timestamp/1000).strftime("%Y-%m-%d %H:%M:%S")}
+                'build_timestamp': datetime.datetime.fromtimestamp(build_timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')}
         response = insert_to_db_by_api(constants.site_builds_rest_api, data)
         if not response.ok:
             raise Exception(f'Couldn\'t insert data: {data} to build table. '
@@ -136,11 +146,12 @@ def insert_into_builds_table(job_id, build_number, build_timestamp):
     return build_id
 
 
-def insert_into_job_results_table(job_id, build_id, build_url, build_result, build_git_sha):
-    job_result_id = get_job_result_id_from_db(job_id, build_id)
+def insert_into_job_results_table(pipeline_id, job_id, build_id, build_url, build_result, build_git_sha):
+    job_result_id = get_job_result_id_from_db(pipeline_id, job_id, build_id)
 
     if not job_result_id:
-        data = {'job_id': job_id,
+        data = {'pipeline_id': pipeline_id,
+                'job_id': job_id,
                 'build_id': build_id,
                 'build_url': build_url,
                 'build_result': Result[build_result].value,
@@ -158,32 +169,74 @@ def get_jobs_info_into_dict(jobs):
     jobs_dict = {}
     for job in jobs:
         job_info = scrap_jenkins_info(job['url'])
+        pipeline_name = job_info['name']
         job_name = job_info['name']
         job_builds = job_info['builds']
-        if job_name not in jobs_dict:
-            jobs_dict[job_name] = []
         for build in job_builds:
+            result = None
             build_git_sha = None
             build_info = scrap_jenkins_info(build['url'])
             for action in build_info['actions']:
+                result = build_info['result']
+                job_number = build_info['number']
                 if 'buildsByBranchName' in action:
                     build_git_sha = action['buildsByBranchName']['refs/remotes/origin/main']['marked']['SHA1']
-            jobs_dict[job_name].append({'build_number': build_info['number'],
-                                        'build_url': build_info['url'],
-                                        'build_result': build_info['result'],
-                                        'build_timestamp': build_info['timestamp'],
-                                        'build_git_sha': build_git_sha})
+            # Detect pipeline
+            if build_info.get('_class').endswith('.WorkflowRun'):
+                pipeline_url = f'{build["url"]}/wfapi'
+                resp = requests.get(pipeline_url).text
+                stages = ast.literal_eval(resp)['stages']
+                for stage in stages:
+                    link = stage['_links']['self']['href']
+                    r = ast.literal_eval(requests.get(f'{constants.jenkins_base_url}/{link}').text)
+                    stage_flow_nodes = r['stageFlowNodes']
+                    for node in stage_flow_nodes:
+                        log_link = node['_links']['log']['href']
+                        req = json.loads(requests.get(f'{constants.jenkins_base_url}/{log_link}').text)
+                        parsed_html = BeautifulSoup(req['text'], 'html.parser')
+                        text = parsed_html.text
+                        text_list = text.split('completed:')
+                        if len(text_list) > 1:
+                            build, completed = text_list[0], text_list[1]
+                            build_str = build.split('Build')[1].strip()
+                            job_name, job_number_internal = build_str.split()
+                            job_name = job_name.strip()
+                            result = completed.strip()
+                            job_number_internal = job_number_internal.split('#')[1].strip()
+                            if pipeline_name not in jobs_dict:
+                                jobs_dict[pipeline_name] = {}
+                            if job_name not in jobs_dict.get(pipeline_name):
+                                jobs_dict[pipeline_name][job_name] = []
+                            jobs_dict[pipeline_name][job_name].append({'build_number': job_number,
+                                                                       'build_url': build_info['url'],
+                                                                       'build_result': result,
+                                                                       'build_timestamp': build_info['timestamp'],
+                                                                       'build_git_sha': build_git_sha,
+                                                                       'log_link': log_link})
+    return jobs_dict
+
+
+def gather_pipelines_info():
+    jobs = scrap_jenkins_info(constants.jenkins_base_url)['jobs']
+    jobs_dict = get_jobs_info_into_dict(jobs)
+
+    for pipeline_name in jobs_dict:
+        pipeline_id = insert_into_pipelines_table(pipeline_name)
+        for job in jobs_dict.get(pipeline_name):
+            job_id = insert_into_jobs_table(pipeline_id, job)
+            for build in jobs_dict.get(pipeline_name).get(job):
+                build_id = insert_into_builds_table(job_id,
+                                                    build['build_number'],
+                                                    build['build_timestamp'])
+                job_result_id = insert_into_job_results_table(pipeline_id, job_id, build_id,
+                                                              build['build_url'],
+                                                              build['build_result'],
+                                                              build['build_git_sha'])
     return jobs_dict
 
 
 def main():
-    jobs = scrap_jenkins_info(constants.jenkins_base_url)['jobs']
-    jobs_dict = get_jobs_info_into_dict(jobs)
-    for job in jobs_dict:
-        job_id = insert_into_jobs_table(job)
-        for build in jobs_dict[job]:
-            build_id = insert_into_builds_table(job_id, build['build_number'], build['build_timestamp'])
-            job_result_id = insert_into_job_results_table(job_id, build_id, build['build_url'], build['build_result'], build['build_git_sha'])
+    jobs_dict = gather_pipelines_info()
 
 
 if __name__ == '__main__':
