d50f5fc9f1d45d589d14ff20b3f36637cb991cc9
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-04-08 19:36:17 +0200

    Updated db config

--- main_app/jenkins_statistics/models.py
+++ main_app/jenkins_statistics/models.py
@@ -2,28 +2,40 @@ import django.utils
 from django.db import models
 
 
+class Pipeline(models.Model):
+    pipeline_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.pipeline_name
+
+
 class Job(models.Model):
-    job_name = models.CharField(max_length=100, unique=True)
+    job_name = models.CharField(max_length=100)
+    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('job_name', 'pipeline')
 
     def __str__(self):
         return self.job_name
 
 
 class Build(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
     build_number = models.IntegerField(default=1)
     build_timestamp = models.DateTimeField(default=django.utils.timezone.now)
 
     class Meta:
-        unique_together = ('job_id', 'build_number')
+        unique_together = ('job', 'build_number')
 
 
 class JobResults(models.Model):
-    job_id = models.ForeignKey(Job, on_delete=models.CASCADE)
-    build_id = models.ForeignKey(Build, on_delete=models.CASCADE)
+    pipeline = models.ForeignKey(Pipeline, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    build = models.ForeignKey(Build, on_delete=models.CASCADE)
     build_url = models.CharField(max_length=100)
     build_result = models.IntegerField(default=1)
-    build_git_sha = models.CharField(max_length=100)
+    build_git_sha = models.CharField(max_length=100, null=True)
 
     class Meta:
-        unique_together = ('job_id', 'build_id')
+        unique_together = ('pipeline', 'job', 'build')
