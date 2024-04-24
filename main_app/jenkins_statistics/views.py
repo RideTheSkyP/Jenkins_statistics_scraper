0c5d41b0dc5883e67bede2667f25041480b41e71
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-04-24 03:05:08 +0200

    Added job failures view

--- main_app/jenkins_statistics/views.py
+++ main_app/jenkins_statistics/views.py
@@ -1,15 +1,17 @@
 import json
 from django.shortcuts import render
 
-from .models import Pipeline, Job, Build, JobResults
+from .models import Pipeline, Job, Build, JobResults, JobFailures
+
+id_to_job_dict = {}
+id_to_build_dict = {}
+id_to_pipeline_dict = {}
+id_to_job_result_dict = {}
+build_numbers_by_pipelines = {}
 
 
 def index(request):
     job_results_dict = {}
-    id_to_pipeline_dict = {}
-    id_to_job_dict = {}
-    id_to_build_dict = {}
-    build_numbers_by_pipelines = {}
     job_results_queryset = JobResults.objects.values()
 
     for job_result in job_results_queryset:
@@ -53,4 +55,37 @@ def test_results(request):
 
 
 def test_failures(request):
-    return render(request, 'test_failures.html', {})
+    job_failures_dict = {}
+    job_failures_queryset = JobFailures.objects.values()
+
+    for job_failure in job_failures_queryset:
+        if not id_to_pipeline_dict.get(job_failure['pipeline_id']):
+            pipeline = Pipeline.objects.get(id=job_failure['pipeline_id'])
+            id_to_pipeline_dict[job_failure['pipeline_id']] = pipeline
+        if not id_to_job_dict.get(job_failure['job_id']):
+            job = Job.objects.get(id=job_failure['job_id'])
+            id_to_job_dict[job_failure['job_id']] = job
+        if not id_to_build_dict.get(job_failure['build_id']):
+            build = Build.objects.get(id=job_failure['build_id'])
+            id_to_build_dict[job_failure['build_id']] = build
+        if not id_to_job_result_dict.get(job_failure['job_result_id']):
+            job_result = JobResults.objects.get(id=job_failure['job_result_id'])
+            id_to_job_result_dict[job_failure['job_result_id']] = job_result
+
+        pipeline = id_to_pipeline_dict.get(job_failure.get('pipeline_id'))
+        job = id_to_job_dict.get(job_failure.get('job_id'))
+        build = id_to_build_dict.get(job_failure.get('build_id'))
+        job_result = id_to_job_result_dict.get(job_failure.get('job_result_id'))
+        if not job_failures_dict.get(pipeline.pipeline_name):
+            job_failures_dict[pipeline.pipeline_name] = {}
+        if not job_failures_dict.get(pipeline.pipeline_name).get(job.job_name):
+            job_failures_dict[pipeline.pipeline_name][job.job_name] = []
+        job_failures_dict[pipeline.pipeline_name][job.job_name].append({'pipeline_name': pipeline.pipeline_name,
+                                                                        'job_name': job.job_name,
+                                                                        'build_number': build.build_number,
+                                                                        'build_result': job_result.build_result,
+                                                                        'build_url': job_result.build_url,
+                                                                        'error_type': job_failure.get('error_type'),
+                                                                        'error_file': job_failure.get('error_file'),
+                                                                        'error_message': job_failure.get('error_message')})
+    return render(request, 'test_failures.html', {'job_failures_dict': job_failures_dict})
