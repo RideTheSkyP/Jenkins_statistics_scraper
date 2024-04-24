01641f937e56faa5afdfa4d0716c93eafce82aca
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-04-24 18:47:17 +0200

    Added custom tags and table with errors

---
+++ main_app/jenkins_statistics/templatetags/extra_tags.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from django.template.defaulttags import register
+
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
+@register.filter
+def trim_last_number_from_url(dictionary, url):
+    return ''.join(url.rsplit('/', 2)[0])
+
+
+class Result(Enum):
+    SUCCESS = 0
+    FAILURE = 1
+    ABORTED = 2
+    UNKNOWN = 2
+
+
+@register.filter
+def convert_result_from_num_to_literal(dictionary, result):
+    return Result(result).name
