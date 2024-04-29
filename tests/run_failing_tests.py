ce766a9ed0d3f5944e4f8980cf92810a917bee5d
Push <49253201+RideTheSkyP@users.noreply.github.com>
2024-04-29 23:38:36 +0200

    Added failing tests to check catching it by script

--- tests/run_failing_tests.py
+++ tests/run_failing_tests.py
@@ -2,6 +2,11 @@ def test_sum():
     assert sum([1, 2, 3]) == 5
 
 
+def test_count_occurrences():
+    assert [1, 2, 3].count(2) == 2
+
+
 if __name__ == '__main__':
     test_sum()
+    test_count_occurrences()
     print('Everything passed')
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,11 @@ def test_sum():
     assert sum([1, 2, 3]) == 6
 
 
+def test_len():
+    assert len([1, 2, 3]) == 2
+
+
 if __name__ == '__main__':
     test_sum()
+    test_len()
     print('Everything passed')
